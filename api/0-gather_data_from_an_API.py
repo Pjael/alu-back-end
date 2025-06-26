347adca505a571d6944ea024a45da092961e9740
Pegdwende Savadogo <p.savadogo@alustudent.com>
2025-06-26 11:07:37 +0200

    usr bin added

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress
 
+#!usr/bin.python3
+
 import requests
 
 class EmployeeToDoList():
