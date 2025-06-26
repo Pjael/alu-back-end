54ffb611268b9acef01357d31a7e7016d504b337
Pegdwende Savadogo <p.savadogo@alustudent.com>
2025-06-26 11:27:45 +0200

    1 strated

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
-#Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress
-
 #!usr/bin/python3
+
+#Using what you did in the task #0, extend your Python script to export data in the CSV format.
 
 import requests
 
