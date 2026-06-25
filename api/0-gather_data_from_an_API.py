c09c4824316190aa101c9850fdd3fd8b2cd75b5a
Pegdwende Savadogo <p.savadogo@alustudent.com>
2026-06-25 10:31:45 +0200

    0-test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
+"""Returns information about a given employee's TODO list progress."""
 
-"""Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-
-import sys
 import requests
+import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 script.py <employee_id>")
-    sys.exit(1)
 
-# Get the employee ID from command line argument and convert it to integer
-try:
-    emp_id = int(sys.argv[1])
-except ValueError:
-    print("Employee ID must be an integer.")
-    sys.exit(1)
-    
-base_url = "https://jsonplaceholder.typicode.com"
-user = requests.get(f"{base_url}/users/{emp_id}").json()
-todos = requests.get(f"{base_url}/todos", params={"userId": emp_id}).json()
+def get_employee_todo_progress(emp_id):
+    """Fetch and display TODO list progress for a given employee ID."""
+    base_url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{base_url}/users/{emp_id}").json()
+    todos = requests.get(
+        f"{base_url}/todos", params={"userId": emp_id}
+    ).json()
 
-employee_name = user.get("name")
-total_tasks = len(todos)
-done_tasks = [task for task in todos if task.get("completed")]
-done_count = len(done_tasks)
-print(employee_name, done_count, total_tasks)
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
+    done_count = len(done_tasks)
 
-print(f"Employee {employee_name} is done with tasks({done_count}/{total_tasks}):")
+    print(
+        f"Employee {employee_name} is done with tasks"
+        f"({done_count}/{total_tasks}):"
+    )
 
-for task in done_tasks:
-    print("\t " + task.get("title"))
+    for task in done_tasks:
+        print("\t " + task.get("title"))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        emp_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    get_employee_todo_progress(emp_id)
